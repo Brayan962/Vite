b82842bb0411d3bf27807804d3693afeb2c57534
Brayan <usuario@DESKTOP-8M2PEVT>
2025-07-23 20:07:39 -0600

    pokedex

---
+++ Pokedex/src/components/panels/HalfPagePanel.jsx
@@ -0,0 +1,19 @@
+const HalfPagePanel = ( {
+    leftNodes,
+    rightNodes,
+    invert = false
+})=>{
+    return (
+        <section className={`half-panel ${invert?'inverted':''}`}>
+            <section>
+                {leftNodes}
+            </section>
+            <section>
+                {rightNodes}
+            </section>
+        </section>
+    )
+}
+
+export default HalfPagePanel;
+
---
+++ Pokedex/src/pages/Pokedex.jsx
@@ -0,0 +1,13 @@
+import PokeList from "../components/pokelist/PokeList";
+import Container from "../components/panels/Container";
+
+const Pokedex = ()=>{
+    return (
+        <Container>
+            <PokeList />
+        </Container>
+    )
+}
+
+export default Pokedex;
+
